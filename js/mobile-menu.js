688f65f02f740f57660c9afa317ac514f878767e
almacoryi <almacoriy@gmail.com>
2021-09-30 01:56:15 +0300

    Кнопка меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,5 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const menuBtnRef = document.querySelector('[data-menu-open]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileBtnClose = document.querySelector('[data-menu-close]');
   menuBtnRef.addEventListener('click', () => {
